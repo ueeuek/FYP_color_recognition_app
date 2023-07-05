7ee3ae9282b828c22d00ad418540fbecf2b0c9d2
ueeuek <138572454+ueeuek@users.noreply.github.com>
2023-07-05 18:51:20 +0800

    Update color_recognition_app.py

--- color_recognition_app.py
+++ color_recognition_app.py
@@ -166,7 +166,7 @@ with c1:
 				st.experimental_rerun()
 
 if st.session_state['HSV_value_text'] is None:
-	text = 'Select any point on the image to know its color !'
+	text = 'Select any point on the image to know its color. You may tilt your phone to view full image !'
 else:
 	text = 'This is <span style="padding: 0px 6px"><strong>' + st.session_state['colour_name_text'].upper() +\
 		'</strong></span> <span style="display: inline-block; width: 30px; height: 18px; background-color:' + st.session_state['colour_name_text'] + '; margin-left: 6px;"></span><br>' +\

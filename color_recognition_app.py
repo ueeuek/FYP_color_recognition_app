4849108f037c0a1a67ef77ae6b32957037f9c212
ueeuek <138572454+ueeuek@users.noreply.github.com>
2023-07-05 14:21:38 +0800

    Update color_recognition_app.py

--- color_recognition_app.py
+++ color_recognition_app.py
@@ -177,7 +177,7 @@ else:
 		colors = row['Color Combination'].split(' and ')
 		other_color = [color for color in colors if color != st.session_state['colour_name_text']][0]
 		category = row['Category']
-		text += f"<span style='padding: 0px 6px'><strong>{other_color.upper()}</strong></span>: {category}<br>"
+		text += f"<span style='display: inline-block; width: 30px; height: 18px; background-color:' + st.session_state['colour_name_text'] + '; margin-left: 6px;'></span><span style='padding: 0px 6px'><strong>{other_color.upper()}</strong></span>: {category}<br>"
 	
 with c2:	
 	st.markdown(

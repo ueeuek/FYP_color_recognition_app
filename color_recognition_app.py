bd39df586194ec0c9b1f144a5d33d6850f1b54e9
ueeuek <138572454+ueeuek@users.noreply.github.com>
2023-07-05 12:24:54 +0800

    Update color_recognition_app.py

--- color_recognition_app.py
+++ color_recognition_app.py
@@ -167,7 +167,7 @@ with c1:
 if st.session_state['HSV_value_text'] is None:
 	text = 'Select any point on the image to know its color !'
 else:
-	text = 'Colour name: <span style=\'background-color: #9685ff; padding-left: 2.5px; padding-right: 2.5px;\'><strong>   ' + st.session_state['colour_name_text'] + '</strong></span><br>' +\
+	text = 'Colour name: <span style=\'background-color: #AEC6CF; padding-left: 2.5px; padding-right: 2.5px;\'><strong>   ' + st.session_state['colour_name_text'] + '</strong></span><br>' +\
 		'Suggested matching colours for ' + st.session_state['colour_name_text'] + ':<br>'
 	# Display st.text elements using stored state
 	for index, row in st.session_state['suggestions_text'].iterrows():

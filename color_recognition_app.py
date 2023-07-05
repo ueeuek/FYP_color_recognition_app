2ced1a64ca7208f5796369947e7f018b5786525b
ueeuek <138572454+ueeuek@users.noreply.github.com>
2023-07-05 13:59:23 +0800

    Update color_recognition_app.py

--- color_recognition_app.py
+++ color_recognition_app.py
@@ -174,17 +174,9 @@ with c1:
 if st.session_state['HSV_value_text'] is None:
 	text = 'Select any point on the image to know its color !'
 else:
-	text = 'This is <span style="background-color: #AEC6CF; padding-left:3px; padding-right:3px; border-radius: 4px;"><strong>' + st.session_state['colour_name_text'] +\
-		'</strong></span> <span style="display: inline-block; width: 20px; height: 18px; background-color:' + st.session_state['colour_name_text'] + '; margin-left: 6px;"></span><br>' +\
+	text = 'This is <span style="background-color: #AEC6CF; padding: 0px 6px; border-radius: 4px;"><strong>' + st.session_state['colour_name_text'] +\
+		'</strong></span> <span style="display: inline-block; width: 30px; height: 18px; background-color:' + st.session_state['colour_name_text'] + '; margin-left: 6px;"></span><br>' +\
 		'Suggested matching colors for ' + st.session_state['colour_name_text'] + ':<br>'
-	#text = 'Color name: <span style="display: inline-flex; align-items: center; background-color: #AEC6CF; padding: 2px 6px; border-radius: 4px;"><strong>' + \
-       	#	st.session_state['colour_name_text'] + '</strong></span> <span style="display: inline-block; width: 12px; height: 12px; background-color:"' + st.session_state['colour_name_text'] + '> </span><br>' + \
-       	#	'Suggested matching colors for ' + st.session_state['colour_name_text'] + ':<br>'
-	#text = 'Color name: <span style="display: inline-flex; align-items: center;"><strong>' + \
-       #st.session_state['colour_name_text'] + '</strong></span> ' + \
-       #'<span style="display: inline-block; width: 12px; height: 12px; background-color: black; margin-left: 6px;"></span><br>' + \
-       #'Suggested matching colors for ' + st.session_state['colour_name_text'] + ':<br>'
-
 
 	# Display st.text elements using stored state
 	for index, row in st.session_state['suggestions_text'].iterrows():

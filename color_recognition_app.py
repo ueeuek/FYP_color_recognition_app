c4d329a95f79b4e48041c0b766bc1fb564ddaca4
ueeuek <138572454+ueeuek@users.noreply.github.com>
2023-07-06 09:24:55 +0800

    Update color_recognition_app.py

--- color_recognition_app.py
+++ color_recognition_app.py
@@ -171,9 +171,9 @@ else:
 	brightness = ''
 	if st.session_state['colour_name_text'] not in ['black', 'white']:
 		Lightness_dict = {10: 'and very dark', 30: 'dark', 50: '', 70: 'light', 90: 'and very light'}
-		Lightness_for_dict = min(Lightness_dict, key=l)
+		Lightness_for_dict = min(Lightness_dict, key=v)
 		brightness = Lightness_dict[Lightness_for_dict]
-		st.write(brightness)
+		#st.write(brightness)
 
 	text = 'This is <span style="padding: 0px 6px"><strong>' + brightness + ' ' + st.session_state['colour_name_text'].upper() +\
 		'</strong></span> <span style="display: inline-block; width: 30px; height: 18px; background-color:' + st.session_state['colour_name_text'] + '; margin-left: 6px;"></span><br>' +\
